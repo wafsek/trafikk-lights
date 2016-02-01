2c96bb855daa871326357a7a62e4f82ac265b22d
wafsek <saraibaljit@gmail.com>
2016-02-01 17:16:44 +0100

    Main.java modified so it starts the server

--- traffic-lights/src/Main.java
+++ traffic-lights/src/Main.java
@@ -1,11 +1,15 @@
 import com.sun.org.apache.xpath.internal.SourceTree;
+import server.TrafficServer;
 
 /**
  * Created by Baljit Singh Sarai on 01.02.16.
+ * @author Baljit Sarai
  */
 public class Main {
 
     public static void main(String[] args){
         System.out.println("Welcome to the traffic light program");
+        TrafficServer server = TrafficServer.getInstance();
+        server.start();
     }
 }
