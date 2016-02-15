628f650458087762725308f6ebbf0f3d61a8af1a
wafsek <saraibaljit@gmail.com>
2016-02-15 15:02:27 +0100

    Added the 'Task' Classes named ServiceTask and ValidateConnections class

---
+++ src/server/ValidateConnections.java
@@ -0,0 +1,20 @@
+package server;
+
+/**
+ * This class will contain the task of validating the handshake of the newly incomming connection
+ * Created by Baljit Sarai on 15.02.16.
+ * @author Baljit Sarai
+ */
+
+
+public class ValidateConnections implements Runnable{
+
+    @Override
+    public void run() {
+        //This method is going to be the methos that validates the connection
+        // If this mathod does more then just the hand shake----we will change the name of
+        // the class :)
+    }
+}
+
+
