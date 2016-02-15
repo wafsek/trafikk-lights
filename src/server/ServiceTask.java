46e031af826a3cd4435aa454dcecb2ddbc0aa904
wafsek <saraibaljit@gmail.com>
2016-02-15 15:02:38 +0100

    Added the 'Task' Classes named ServiceTask and ValidateConnections class

--- src/server/ServiceTask.java
+++ src/server/ServiceTask.java
@@ -1,7 +1,14 @@
 package server;
 
 /**
- * Created by wafsek on 15.02.16.
+ * Created by Baljit Sarai on 15.02.16.
+ * @author Baljit Sarai
  */
-public class ServiceTask {
+public class ServiceTask implements Runnable{
+
+    @Override
+    public void run() {
+        //This is the method that is going to go thro all the connections in
+        // the validate "bucket" and send the pending signal
+    }
 }
--- src/server/TrafficService.java
+++ src/server/TrafficService.java
@@ -8,7 +8,7 @@ package server;
 
 
 /**
- * 
+ *
  */
 public class TrafficService {
 
